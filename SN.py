cc2d3707fdb83bcd18f40db360ec43c39fd5c11f
sajedeh <151438159+sajdemhajr@users.noreply.github.com>
2024-06-26 02:46:01 +0430

    CREATING ARTICLES TABLE AND WRITERS TABLE

--- SN.py
+++ SN.py
@@ -58,3 +58,34 @@ try:
     mycursor.execute(query)
 except Exception as e:
     print(e)
+
+#CREATE_TABLE_WRITERS
+query = ("""CREATE TABLE Writers (
+    code VARCHAR(50) ,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) ,
+    bio TEXT,
+    PRIMARY KEY(code)
+)
+""")
+try:
+    mycursor.execute(query)
+except Exception as e:
+    print(e)
+
+#CREATE_TABLE_ARTICLES
+query = ("""CREATE TABLE Articles (
+    content TEXT NOT NULL,
+    keywords VARCHAR(255),
+    title VARCHAR(100),
+    category VARCHAR(50),
+    writer_code VARCHAR(50) NOT NULL,
+    writer_name VARCHAR(100),
+    writer_email VARCHAR(100),
+    FOREIGN KEY (writer_code) REFERENCES Writers(code)
+)
+""")
+try:
+    mycursor.execute(query)
+except Exception as e:
+    print(e)
