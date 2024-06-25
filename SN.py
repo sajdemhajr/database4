5b485394be35bb9c45411ef414f829971029a980
sajedeh <151438159+sajdemhajr@users.noreply.github.com>
2024-06-26 02:32:17 +0430

    Creating photographers table and images table

--- SN.py
+++ SN.py
@@ -9,8 +9,52 @@ try:
     mycursor.execute()
 except Exception as e:
     print(e)
+
+#CERATE_DATABASE
 sj = "CREATE DATABASE db_class"
 try:
     mycursor.execute(sj)
 except Exception as e :
+    print(e)
+
+#CONNECT_TO_DATABASE
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="S@jedeh_1382",
+    database = "db_class"
+)
+mycursor = mydb.cursor()
+
+#CREATE_TABLE_PHOTOGRAFERS
+query = ("""CREATE TABLE photographers (
+    code VARCHAR(50) ,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) ,
+    bio TEXT,
+    PRIMARY KEY(code)
+)
+""")
+try:
+    mycursor.execute(query)
+except Exception as e:
+    print(e)
+
+#CREATE_TABLE_IMAGES
+query = """CREATE TABLE Images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_path VARCHAR(255) NOT NULL,
+    title VARCHAR(100),
+    tags VARCHAR(255),
+    description TEXT,
+    category VARCHAR(50),
+    photographer_code VARCHAR(50) NOT NULL,
+    photographer_name VARCHAR(100),
+    photographer_email VARCHAR(100),
+    FOREIGN KEY (photographer_code) REFERENCES photographers(code)
+)
+"""
+try:
+    mycursor.execute(query)
+except Exception as e:
     print(e)
