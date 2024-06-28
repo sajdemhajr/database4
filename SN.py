29b1ad4ffbc4d51d4a6204582a74e8f8a32e2ab4
sajedeh <151438159+sajdemhajr@users.noreply.github.com>
2024-06-28 15:21:03 +0430

    INSERTING CSV DATAS TO IMAGES AND ARTICLES TABLES.(the problem is that every time the code is run, duplicated data is added again.)

--- SN.py
+++ SN.py
@@ -43,7 +43,6 @@ except Exception as e:
 
 #CREATE_TABLE_IMAGES
 query = """CREATE TABLE Images (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     image_path VARCHAR(255) NOT NULL,
     title VARCHAR(100),
     tags VARCHAR(255),
@@ -90,21 +89,25 @@ def insert_users(code, name, email):
     except Exception as e:
         print(e)
 
+#INSERT_TO_IMAGES
+def insert_images(image_path,title,tags,description,category,photographer_code,photographer_name,photographer_email):
+    query = """INSERT INTO images (image_path,title,tags,description,category,photographer_code,photographer_name,photographer_email) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s) """
+    data = (image_path,title,tags,description,category,photographer_code,photographer_name,photographer_email)
+    try:
+        mycursor.execute(query, data)
+    except Exception as e:
+        print(e)
 
-#READ_FROM_IMAGE_CSV_FILE
-try:
-    with open('images.csv', 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            insert_users(
-                row['photographer_code'],
-                row['photographer_name'],
-                row['photographer_email'], 
-            )
-except Exception as e:
-    print(e)
-else:
-      mydb.commit()
+#INSERT_TO_ARTCLES
+def insert_articles(content,keywords,title,category,writer_code,writer_name,writer_email):
+    query = """INSERT INTO articles (content,keywords,title,category,writer_code,writer_name,writer_email) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s) """
+    data = (content,keywords,title,category,writer_code,writer_name,writer_email)
+    try:
+        mycursor.execute(query, data)
+    except Exception as e:
+        print(e)
 
 #READ_FROM_ARTICLES_CSV_FILE
 try:
@@ -115,6 +118,40 @@ try:
                 row['writer_code'],
                 row['writer_name'],
                 row['writer_email']
+            )
+            insert_articles(
+                row['content'],
+                row['keywords'],
+                row['title'],
+                row['category'],
+                row['writer_code'],
+                row['writer_name'],
+                row['writer_email']
+            )
+except Exception as e:
+    print(e)
+else:
+    mydb.commit()
+        
+#READ_FROM_IMAGES_CSV_FILE
+try:
+    with open('images.csv', 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            insert_users(
+                row['photographer_code'],
+                row['photographer_name'],
+                row['photographer_email']
+            )
+            insert_images(
+                row['image_path'],
+                row['title'],
+                row['tags'],
+                row['description'],
+                row['category'],
+                row['photographer_code'],
+                row['photographer_name'],
+                row['photographer_email']
             )
 except Exception as e:
     print(e)
